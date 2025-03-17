a069707131ce85a2c88748f303749d6c3e195e48
Kietntse173576 <boibitcute@gmail.com>
2025-03-17 22:09:05 +0700

    fix get student contentManager paginated

--- src/app/(pages)/user-management/_components/cell-action.tsx
+++ src/app/(pages)/user-management/_components/cell-action.tsx
@@ -1,14 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { User } from "@/types/user";
+import { useRouter } from "next/navigation";
 
 interface CellActionProps {
   data: User;
 }
 
-export const CellAction: React.FC<CellActionProps> = () => {
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const router = useRouter();
+
+  const handleViewUser = () => {
+    router.push(`/user-management/${data.userId}`);
+  };
+  
+
   return (
-    <>
-        <Button> View</Button>
-    </>
+    <Button onClick={handleViewUser} variant="outline">
+      View
+    </Button>
   );
 };
